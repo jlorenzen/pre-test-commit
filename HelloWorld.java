82c327b6ffb2a8467e68061a8f4df370f4c15461
jlorenzen <jameslorenzen@gmail.com>
2010-08-12 23:38:41 -0500

    removed bad lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Approved Change123");
-        System.out.println("Approved Change123");
     }
 }
