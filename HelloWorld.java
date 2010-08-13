340ae555d4cea192513f25ca239db90a1357f4ea
jlorenzen <jameslorenzen@gmail.com>
2010-08-12 23:36:09 -0500

    first approved change 123 new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Approved Change123");
+        System.out.println("Approved Change123");
     }
 }
